694fae4da255a3f90482dfdea7bca7d5c95657d4
Anas8e <94750646+Anas8e@users.noreply.github.com>
2023-05-25 09:48:44 +0100

    again and again

--- execute.c
+++ execute.c
@@ -22,6 +22,28 @@ void execute_command(char *command)
  * handle_child_process - Handle the child process
  * @command: The command to execute
  */
+/**
+ * execute_command - Execute the given command
+ * @command: The command to execute
+ */
+void execute_command(char *command)
+{
+	if (fork() == 0)
+	{
+		/* Child process */
+		handle_child_process(command);
+	}
+	else
+	{
+		/* Parent process */
+		handle_parent_process();
+	}
+}
+
+/**
+ * handle_child_process - Handle the child process
+ * @command: The command to execute
+ */
 void handle_child_process(char *command)
 {
 	pid_t pid;
@@ -52,7 +74,6 @@ void handle_child_process(char *command)
 		execute_parent_process(pipefd);
 	}
 }
-
 /**
  * execute_child_process - Execute the child process
  * @command: The command to execute
@@ -79,8 +100,6 @@ void execute_parent_process(int pipefd[])
 {
 	char output[BUFFER_SIZE];
 	int nbytes;
-	int total_bytes = 0;
-	char buffer[BUFFER_SIZE];
 	int buffer_size = 0;
 
 	close(pipefd[1]);
